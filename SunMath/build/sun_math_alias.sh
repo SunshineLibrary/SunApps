af2ae0ffcd656e3ff20329771c6aeb40e541a8b5
lishoubo <litttlan@gmail.com>
2012-07-16 16:02:41 +0800

    [yanan, shoubo] rename the project

--- SunMath/build/sun_math_alias.sh
+++ SunMath/build/sun_math_alias.sh
@@ -6,7 +6,7 @@ MATH_APP_ACTIVITY="com.ssl.curriculum.math.activity.MainActivity"
 
 MATH_PROJECT_DIR=$1
 
-alias install_math_app_to_multi_devices="sh ./build/install_to_multi_devices.sh ./bin/SslMathApp-release.apk"
+alias install_math_app_to_multi_devices="sh ./build/install_to_multi_devices.sh ./bin/SunMathApp-release.apk"
 alias clean_math_app_app='cd $MATH_PROJECT_DIR&&ant clean'
 
 alias math_release='clean_math_app_app&&cd $MATH_PROJECT_DIR&&ant release&&install_math_app_to_multi_devices $MATH_APP_PACKAGE $MATH_APP_ACTIVITY'
