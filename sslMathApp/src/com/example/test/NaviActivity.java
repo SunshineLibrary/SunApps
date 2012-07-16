556a9be4d65c7f42dd2d04b7efd3d790bdd2f4f0
Yanan Guo <gynsolomon@gmail.com>
2012-07-16 12:04:08 +0800

    [yanan, shoubo] add sslMathApp

---
+++ sslMathApp/src/com/example/test/NaviActivity.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import android.os.Bundle;
+import android.app.Activity;
+public class NaviActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navi);
+    }
+    
+}
