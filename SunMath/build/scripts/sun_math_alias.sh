14cfa717bdab7885319723febc9de3cd86766d0a
lishoubo <litttlan@gmail.com>
2012-07-18 15:37:20 +0800

    [jim, shoubo] add unit test task to scripts

--- SunMath/build/scripts/sun_math_alias.sh
+++ SunMath/build/scripts/sun_math_alias.sh
@@ -10,3 +10,4 @@ alias install_math_app_to_multi_devices="sh ./build/scripts/install_to_multi_dev
 alias clean_math_app_app='cd $MATH_PROJECT_DIR&&ant clean'
 
 alias math_release='clean_math_app_app&&cd $MATH_PROJECT_DIR&&ant release&&install_math_app_to_multi_devices $MATH_APP_PACKAGE $MATH_APP_ACTIVITY'
+alias math_ut='clean_math_app_app&&cd $MATH_PROJECT_DIR&&ant unit_test'
