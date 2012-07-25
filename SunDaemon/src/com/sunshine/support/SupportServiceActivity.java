1f389469a46dc13dd49f778e5599ec80c10efb14
lishoubo <litttlan@gmail.com>
2012-07-25 13:37:16 +0800

    [zhengwang, shoubo] add more test imgaes

--- SunDaemon/src/com/sunshine/support/SupportServiceActivity.java
+++ SunDaemon/src/com/sunshine/support/SupportServiceActivity.java
@@ -21,6 +21,11 @@ public class SupportServiceActivity extends Activity {
 //        localPackageManager = this.getPackageManager();
 //        serverClient = new WebClient();
         setContentView(R.layout.main);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
         initFileStorage();
     }
 
@@ -30,6 +35,13 @@ public class SupportServiceActivity extends Activity {
         imageTestData.prepareDataForGallery("image02.jpeg", "thumbnail02.jpg", R.drawable.image02, R.drawable.thumbnail02, "test 02");
         imageTestData.prepareDataForGallery("image03.jpeg", "thumbnail03.jpg", R.drawable.image03, R.drawable.thumbnail03, "test 03");
         imageTestData.prepareDataForGallery("image04.jpeg", "thumbnail04.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 04");
+        imageTestData.prepareDataForGallery("image05.jpeg", "thumbnail05.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 05");
+        imageTestData.prepareDataForGallery("image06.jpeg", "thumbnail06.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 06");
+        imageTestData.prepareDataForGallery("image07.jpeg", "thumbnail07.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 07");
+        imageTestData.prepareDataForGallery("image08.jpeg", "thumbnail08.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 08");
+        imageTestData.prepareDataForGallery("image09.jpeg", "thumbnail09.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 09");
+        imageTestData.prepareDataForGallery("image10.jpeg", "thumbnail10.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 09");
+        imageTestData.prepareDataForGallery("image11.jpeg", "thumbnail11.jpg", R.drawable.image04, R.drawable.thumbnail04, "test 09");
     }
 
 
