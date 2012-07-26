1c99e1ef3a892c4d749e7bf9c66ac0f7e1cef213
Jim Chen <jmchn1994@gmail.com>
2012-07-26 16:48:59 +0800

    [jim] merge MainActivity and comment out gallery loader

--- SunMath/src/com/ssl/curriculum/math/activity/MainActivity.java
+++ SunMath/src/com/ssl/curriculum/math/activity/MainActivity.java
@@ -37,11 +37,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-<<<<<<< HEAD
         //loadGalleryContent();
-=======
-//        loadGalleryContent();
->>>>>>> 6fc2aef781741152d81dcc148ef3d4e624c938ec
     }
 
     private void loadGalleryContent() {
