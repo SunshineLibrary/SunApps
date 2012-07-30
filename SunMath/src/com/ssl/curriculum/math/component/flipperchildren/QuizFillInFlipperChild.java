bbb41342495dad0ff27bb9e8211062a87739e623
lishoubo <litttlan@gmail.com>
2012-07-30 19:04:17 +0800

    [zhengw, shoubo] show quiz on web view

--- SunMath/src/com/ssl/curriculum/math/component/flipperchildren/QuizFillInFlipperChild.java
+++ SunMath/src/com/ssl/curriculum/math/component/flipperchildren/QuizFillInFlipperChild.java
@@ -1,26 +1,19 @@
 package com.ssl.curriculum.math.component.flipperchildren;
 
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
-import android.webkit.WebChromeClient;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
-import android.widget.Toast;
 import com.ssl.curriculum.math.R;
 
 public class QuizFillInFlipperChild extends LinearLayout {
-
     private WebView questionWebView;
-    private ProgressDialog progressDialog;
 
     public QuizFillInFlipperChild(Context context) {
         super(context);
         initUI();
         initWebView();
-        loadURL();
     }
 
     private void initUI() {
@@ -29,54 +22,12 @@ public class QuizFillInFlipperChild extends LinearLayout {
         addView(viewGroup);
         questionWebView = (WebView) findViewById(R.id.quiz_fill_in_flipper_child_question);
     }
+
     private void initWebView() {
         questionWebView.getSettings().setJavaScriptEnabled(true);
+        questionWebView.getSettings().setAllowFileAccess(true);
+        questionWebView.getSettings().setDomStorageEnabled(true);
         questionWebView.setScrollBarStyle(0);
-        questionWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-                Toast.makeText(getContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        questionWebView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress == 100) {
-                    hideProgressBar();
-                }
-                super.onProgressChanged(view, newProgress);
-            }
-
-
-        });
-
-        progressDialog = new ProgressDialog(getContext());
-        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-        progressDialog.setMessage("数据载入中，请稍候！");
-    }
-
-    private void loadURL() {
-        questionWebView.loadUrl("http://baidu.com");
-        showProgressBar();
-    }
-
-    private void showProgressBar() {
-        post(new Runnable() {
-            @Override
-            public void run() {
-                progressDialog.show();
-            }
-        });
-    }
-
-    private void hideProgressBar() {
-        post(new Runnable() {
-            @Override
-            public void run() {
-                progressDialog.hide();
-            }
-        });
-
+        questionWebView.loadUrl("file:///android_asset/sample-asciimath.html");
     }
 }
