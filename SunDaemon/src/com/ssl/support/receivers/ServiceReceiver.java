478fd70c02d7b6cda69d052a627346d048998f5a
Bowen Sun <ukisami@gmail.com>
2012-09-18 16:35:44 +0800

    [bowen] update service runs on connectivity change

--- SunDaemon/src/com/ssl/support/receivers/ServiceReceiver.java
+++ SunDaemon/src/com/ssl/support/receivers/ServiceReceiver.java
@@ -9,17 +9,18 @@ import com.ssl.support.services.UpdateService;
 
 public class ServiceReceiver extends BroadcastReceiver {
 
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		
-		if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
-			context.startService(new Intent(context, APISyncService.class));
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            context.startService(new Intent(context, APISyncService.class));
             context.startService(new Intent(context, UpdateService.class));
-		} else if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-			boolean isDisconnected = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
-			if (!isDisconnected) {
-				context.startService(new Intent(context, APISyncService.class));
-			}
-		}
-	}
+        } else if (intent.getAction().equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
+            boolean isDisconnected = intent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false);
+            if (!isDisconnected) {
+                context.startService(new Intent(context, APISyncService.class));
+                context.startService(new Intent(context, UpdateService.class));
+            }
+        }
+    }
 }
