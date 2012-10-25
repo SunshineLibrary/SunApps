c43e99e990a785679ab418b6c377c65b0692d847
qinchong <qinchong1988@gmail.com>
2012-10-25 19:12:17 +0800

    unzip and present html files

--- SunMath/src/com/ssl/curriculum/math/activity/WebViewActivity.java
+++ SunMath/src/com/ssl/curriculum/math/activity/WebViewActivity.java
@@ -1,27 +1,41 @@
 package com.ssl.curriculum.math.activity;
 
+import java.io.File;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
 import android.webkit.WebView;
+
 import com.ssl.curriculum.math.R;
+import com.ssl.curriculum.math.utils.AssetCopyUtil;
+import com.ssl.curriculum.math.utils.UnpackZipUtil;
 
 /**
- * Created with IntelliJ IDEA.
- * User: mendlin
- * Date: 12-8-14
- * Time: 下午4:44
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: mendlin Date: 12-8-14 Time: 下午4:44 To
+ * change this template use File | Settings | File Templates.
  */
 public class WebViewActivity extends Activity {
-    private WebView webView;
+	private WebView webView;
+	private String loadUrl;
+	private ProgressDialog dialog;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.webview_layout);
-
-        webView = (WebView) findViewById(R.id.webview_main);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.loadUrl("http://42.121.65.247");
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		dialog = new ProgressDialog(this);
+		dialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+		setContentView(R.layout.webview_layout);
+		
+		loadUrl = "file:///"+getIntent().getCharSequenceExtra("indexPath");
+		
+		webView = (WebView) findViewById(R.id.webview_main);
+		webView.getSettings().setJavaScriptEnabled(true);
+	 
+		dialog.dismiss();
+		webView.loadUrl("file:///" + loadUrl);
+		System.out.println(loadUrl);
+	}
 }
