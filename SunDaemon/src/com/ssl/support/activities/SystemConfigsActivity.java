2af3f773ec1d7aab69430a507b0f6aa0dd2b07d7
linfeng <alexyangbg@gmail.com>
2012-11-15 01:23:11 +0800

    add version code for daemon

--- SunDaemon/src/com/ssl/support/activities/SystemConfigsActivity.java
+++ SunDaemon/src/com/ssl/support/activities/SystemConfigsActivity.java
@@ -1,17 +1,33 @@
 package com.ssl.support.activities;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.Preference;
 import com.ssl.support.daemon.R;
+import android.content.pm.PackageInfo;
 
 /**
  * @author Bowen Sun
  * @version 1.0
  */
 public class SystemConfigsActivity extends PreferenceActivity{
+
+    private Preference mVersionNamePref;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        PackageInfo pinfo = null;
+        try {
+            pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        String versionName = pinfo.versionName;
+        mVersionNamePref = findPreference("version_name");
+        mVersionNamePref.setSummary(versionName);
     }
 }
